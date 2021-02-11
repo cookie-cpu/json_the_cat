1ecfb4ff197aec2fda753b07f82fdaeb660fdef4
Matthew Cook <matthewcook13987@gmail.com>
2021-02-11 21:01:54 +0000

    Used eslint to clean up code.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,20 @@
-const request = require('request')
+const request = require('request');
 
 let input = process.argv.splice(2);
 
 let catName = input[0];
 
-let address = `https://api.thecatapi.com/v1/breeds/search/?q=${catName}`
-
+let address = `https://api.thecatapi.com/v1/breeds/search/?q=${catName}`;
 
 
 request(address, (error, response, body) => {
-  if (error){
-    console.log(`Error: ${error}`)
-  } 
-  else if (body === "[]"){
-    console.log("Breed not found")
-  }
-  else {
+  if (error) {
+    console.log(`Error: ${error}`);
+  } else if (body === "[]") {
+    console.log("Breed not found");
+  } else {
     const data = JSON.parse(body);
-    console.log(data[0].description)
+    console.log(data[0].description);
   }
-})
+});
 
