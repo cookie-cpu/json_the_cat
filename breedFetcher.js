f26491c75fa8dd2da265fd36f5d17f4d09f618c8
Matthew Cook <matthewcook13987@gmail.com>
2021-02-11 20:59:02 +0000

    Added a conditional to return breed not found if an empty dataset is receieved.

--- breedFetcher.js
+++ breedFetcher.js
@@ -7,14 +7,17 @@ let catName = input[0];
 let address = `https://api.thecatapi.com/v1/breeds/search/?q=${catName}`
 
 
+
 request(address, (error, response, body) => {
   if (error){
     console.log(`Error: ${error}`)
-  }
-  if (body.length === 0){
+  } 
+  else if (body === "[]"){
     console.log("Breed not found")
   }
-  const data = JSON.parse(body);
-  console.log(data[0].description)
-  
+  else {
+    const data = JSON.parse(body);
+    console.log(data[0].description)
+  }
 })
+
